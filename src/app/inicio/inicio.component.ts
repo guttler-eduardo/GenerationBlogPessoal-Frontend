ab53499a793a78f43dfc6a40c75efa106803ea82
unknown <contact.guttler@gmail.com>
2022-06-10 22:40:01 -0300

    POST componente postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +14,17 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  postagem: Postagem = new Postagem()
+  listaTemas: Tema[]
+  idTema: number
+  user: User = new User()
+  idUser = environment.id
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -18,6 +32,30 @@ export class InicioComponent implements OnInit {
       //alert('Sua sessão expirou, faça o login novamente!')
       this.router.navigate(['/login'])
     }
+    this.getAllTemas()
   }
 
+  getAllTemas() {
+    this.temaService.getAllTemas().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema() {
+    this.temaService.getByIdTemas(this.idTema).subscribe((resp: Tema) => {
+      this.tema = resp
+    })
+  }
+
+  publish() {
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+    })
+  }
 }
